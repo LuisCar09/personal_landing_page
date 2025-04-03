c1c8d2177624f5f3476f9c9180ee34713dbbe3b4
Luis Carlos <luiscbravo94@gmail.com>
2025-04-03 17:20:41 +0200

    style: agregada animation al titulo

--- personal_landing/src/pages/Home.jsx
+++ personal_landing/src/pages/Home.jsx
@@ -1,25 +1,52 @@
 import React from 'react';
+import { FaCode, FaShieldAlt, FaLaptopCode } from 'react-icons/fa';
 import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <section className="hero-section">
-      <div className="hero-content">
-        <div className="profile-container">
-          <img 
-            src="/images/profile.jpg" 
-            alt="Luis Carlos Bravo" 
-            className="profile-image"
-          />
+    <div className="home">
+      <section className="hero-section">
+        <div className="hero-content">
+          <div className="title-container">
+            <h1 className="name">
+              <span className="letter">L</span>
+              <span className="letter">u</span>
+              <span className="letter">i</span>
+              <span className="letter">s</span>
+              <span className="space">&nbsp;</span>
+              <span className="letter">C</span>
+              <span className="letter">a</span>
+              <span className="letter">r</span>
+              <span className="letter">l</span>
+              <span className="letter">o</span>
+              <span className="letter">s</span>
+              <span className="space">&nbsp;</span>
+              <span className="letter">B</span>
+              <span className="letter">r</span>
+              <span className="letter">a</span>
+              <span className="letter">v</span>
+              <span className="letter">o</span>
+            </h1>
+
+            <ul className="roles-list">
+            <li style={{ '--i': 1 }}>
+              <FaCode className="role-icon" />
+              Software Developer
+            </li>
+            <li style={{ '--i': 2 }}>
+              <FaLaptopCode className="role-icon" />
+              Technology Enthusiast
+            </li>
+            <li style={{ '--i': 3 }}>
+              <FaShieldAlt className="role-icon" />
+              Cybersecurity Integration
+            </li>
+          </ul>
+          </div>
+          
         </div>
-        <h1>Luis Carlos Bravo</h1>
-        <ul className="roles-list">
-          <li>Software developer</li>
-          <li>Technology enthusiast</li>
-          <li>Cybersecurity Integration</li>
-        </ul>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
