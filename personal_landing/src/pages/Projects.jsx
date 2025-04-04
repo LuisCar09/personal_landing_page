a2a0dbfc1c4e02a6e90becb2fbab0df2b3b62198
Luis Carlos <luiscbravo94@gmail.com>
2025-04-04 20:07:22 +0200

    feat: added project images

--- personal_landing/src/pages/Projects.jsx
+++ personal_landing/src/pages/Projects.jsx
@@ -36,7 +36,7 @@ const Projects = () => {
       title: "Futurabiolab",
       description: "FuturaBioLab is a collaborative project focused on innovation with biomaterials and its impact on the open-source community.",
       technologies: ["React", "Node.js", "MongoDB"],
-      image: "https://via.placeholder.com/600x400",
+      image: "https://dl.dropboxusercontent.com/scl/fi/qgyxhm3sj1onuj5y05js3/futura.png?rlkey=g49h21mincgqnhuzi5vbbgy3e&st=payrhic3",
       github: "https://github.com/luiscar09/futurabiolab",
       demo: "https://futurabiolab2.netlify.app/"
     },
@@ -44,7 +44,7 @@ const Projects = () => {
       title: "Web Store",
       description: "FuturaWebStore is an e-commerce platform built with Node.js for frontend rendering and MongoDB as the backend database.",
       technologies: ["Node.js", "MongoDB", "Express"],
-      image: "https://via.placeholder.com/600x400",
+      image: "https://dl.dropboxusercontent.com/scl/fi/aijdug4kxlyuznrsgmowv/backproject.png?rlkey=0d3wx1ixu4l1mbz69r17zo0mz&st=gvaddfx8",
       github: "https://github.com/luiscar09/webstore",
       demo: "https://backend-project-break-c2xv.onrender.com/"
     },
@@ -52,7 +52,7 @@ const Projects = () => {
       title: "Burger Shop",
       description: "BurgerShop is a commerce platform built with React, consuming its own API developed with Node.js.",
       technologies: ["React", "Node.js", "MongoDB"],
-      image: "https://via.placeholder.com/600x400",
+      image: "https://dl.dropboxusercontent.com/scl/fi/q3ob0i5dvoybxtofziv60/burgershop.png?rlkey=89n20g5k46oui5xrpyj0b73tt&st=grar7qov&dl=0",
       github: "https://github.com/luiscar09/burgershop",
       demo: "https://burgershopstore.netlify.app/"
     },
@@ -60,7 +60,7 @@ const Projects = () => {
       title: "Catch The Insect",
       description: "Catch The Insect is a game built with JavaScript, CSS, and HTML5.",
       technologies: ["JavaScript", "HTML5", "CSS"],
-      image: "https://via.placeholder.com/600x400",
+      image: "https://dl.dropboxusercontent.com/scl/fi/95s5t1k66w72j42u9kzqo/insectos.png?rlkey=anhne0vt47hl99dvdaqiy1z9h&st=guoqm5ev",
       github: "https://github.com/luiscar09/Catch-The-Insect",
       demo: "https://luiscar09.github.io/Catch-The-Insect/"
     },
@@ -68,7 +68,7 @@ const Projects = () => {
       title: "Movie App",
       description: "Movie App is a web application built with JavaScript, CSS, and HTML5, consuming an API to render movies.",
       technologies: ["JavaScript", "HTML5", "CSS"],
-      image: "https://via.placeholder.com/600x400",
+      image: "https://dl.dropboxusercontent.com/scl/fi/30dxj906djl9oln8pdb38/movieapp.png?rlkey=pwk38ppu5aqsoohyisu2j6lxg&st=hm5lfck4",
       github: "https://github.com/luiscar09/movie-app",
       demo: "https://luiscar09.github.io/movie-app/"
     },
@@ -76,7 +76,7 @@ const Projects = () => {
       title: "Drink Water",
       description: "Drink Water is a game developed with JavaScript, CSS, and HTML5 to practice programming logic.",
       technologies: ["JavaScript", "HTML5", "CSS"],
-      image: "https://via.placeholder.com/600x400",
+      image: "https://dl.dropboxusercontent.com/scl/fi/5ou2gtz3wfpxvzwanka6g/drink.png?rlkey=5yummcm7dl3yt39hm9hmt50eu&st=1aj1ps4m",
       github: "https://github.com/luiscar09/Drink-Water",
       demo: "https://luiscar09.github.io/Drink-Water/"
     }
