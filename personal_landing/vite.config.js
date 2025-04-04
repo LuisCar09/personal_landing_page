0df74e7ae37bccc92829c98de5ffe3e4f8bc89ae
Luis Carlos <luiscbravo94@gmail.com>
2025-04-04 19:10:13 +0200

    feat: change vite.config.js

--- personal_landing/vite.config.js
+++ personal_landing/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['personal-landing-page-ffa4.onrender.com']
+  }
 })
